88ca8d7fad667489767d1da46c100a7a79ed8e07
Alexey Kulish <alexkulish1993@gmail.com>
2022-01-05 20:50:29 +0300

    Setup custom cell in Storyboard Setup logic for Model, DevUITableVC Setup outlets for DevTableViewCell Add images

--- HwSportLeague/Controllers/DevelopersTableViewCell.swift
+++ HwSportLeague/Controllers/DevelopersTableViewCell.swift
@@ -8,16 +8,9 @@
 import UIKit
 
 class DevelopersTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    @IBOutlet var developerImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var roleLabel: UILabel!
+    
 }
--- HwSportLeague/Models/DeveloperModel/DeveloperModel.swift
+++ HwSportLeague/Models/DeveloperModel/DeveloperModel.swift
@@ -5,5 +5,16 @@
 //  Created by Alex Kulish on 05.01.2022.
 //
 
-import Foundation
+struct Developer {
+    let logo: String
+    let name: String
+    let role: String
+}
 
+extension Developer {
+    
+    static func getDeveloper() -> [Developer] {
+        [Developer(logo: "Image2", name: "Sergey", role: "Team Lead"),
+         Developer(logo: "Image", name: "Alexey", role: "Junior developer")]
+    }
+}
