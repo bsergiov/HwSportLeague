1635c1ae8bb6ee079863828e887e019b6caad864
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-01-05 23:29:51 +0200

    added StandingViewController.swift

---
+++ HwSportLeague/Controllers/StandingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StandingViewController.swift
+//  HwSportLeague
+//
+//  Created by BSergio on 05.01.2022.
+//
+
+import UIKit
+
+class StandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
