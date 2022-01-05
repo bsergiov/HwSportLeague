1c01939354893861af7aeb01f1f42803d37a9d6f
Alexey Kulish <alexkulish1993@gmail.com>
2022-01-05 16:17:26 +0300

    Add new files: DevStoryboard, DevUITableVC, DevUITableViewCell, DevModel

---
+++ HwSportLeague/Controllers/DevelopersTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  HwSportLeague
+//
+//  Created by Alex Kulish on 05.01.2022.
+//
+
+import UIKit
+
+class DevelopersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HwSportLeague/Models/DeveloperModel/DeveloperModel.swift
@@ -0,0 +1,9 @@
+//
+//  DeveloperModel.swift
+//  HwSportLeague
+//
+//  Created by Alex Kulish on 05.01.2022.
+//
+
+import Foundation
+
