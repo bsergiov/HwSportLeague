23855732d5875c2841a1ea7eb242a4db7ca390c8
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-01-08 10:26:42 +0200

    added graphic screen games

--- HwSportLeague/Controllers/GameTableViewCell.swift
+++ HwSportLeague/Controllers/GameTableViewCell.swift
@@ -16,9 +16,18 @@ class GameTableViewCell: UITableViewCell {
     @IBOutlet weak var homeNameLabel: UILabel!
     @IBOutlet weak var awayNameLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    // MARK: - Public Properties
+    var gameModel: GameModel?
+    
+    func setupCell() {
         
-    }
+        guard let gameModel = gameModel else { return }
+        
+        logoHomeImagView.image = UIImage(named: gameModel.homeTeam)
+        logoAwayImagView.image = UIImage(named: gameModel.awayTeam)
 
+        homeNameLabel.text = gameModel.homeTeam
+        awayNameLabel.text = gameModel.awayTeam
+    }
+ 
 }
--- HwSportLeague/Controllers/MainViewController.swift
+++ HwSportLeague/Controllers/MainViewController.swift
@@ -9,14 +9,37 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    // MARK: - Private Properties
+    let games = GameModel.getGames()
+    
     // MARK: - Life Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(GameModel.getGames())
     }
     
 // MARK: - IB Action
     @IBAction func segmentControl(_ sender: Any) {
+    }
+    
+}
+
+// MARK: - TableVeiw Protocols
+extension MainViewController: UITableViewDataSource, UITabBarDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GameTableViewCell", for: indexPath) as! GameTableViewCell
+        
+        cell.gameModel = games[indexPath.row]
+        
+        cell.setupCell()
+        
+        return cell
     }
     
 }
