238b3614effe158dbea7b47b2650ce66147ba929
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-01-08 12:18:33 +0200

    added segmented login and show teams screen

--- HwSportLeague/Controllers/MainViewController.swift
+++ HwSportLeague/Controllers/MainViewController.swift
@@ -8,38 +8,72 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     // MARK: - IB Outlets
     @IBOutlet weak var mainTableView: UITableView!
     
     // MARK: - Private Properties
     let games = GameModel.getGames()
+    let teams = TeamDataManager.teams
+    
+    var stateTableVeiw = StateTableView.games
     
     // MARK: - Life Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
-// MARK: - IB Action
-    @IBAction func segmentControl(_ sender: Any) {
+    // MARK: - IB Action
+    @IBAction func gamesOrTeamsChanger(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            stateTableVeiw = .games
+        default:
+            stateTableVeiw = .teams
+        }
+        mainTableView.reloadData()
     }
-    
 }
 
 // MARK: - TableVeiw Protocols
 extension MainViewController: UITableViewDataSource, UITabBarDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        games.count
+        
+        switch stateTableVeiw {
+        case .games:
+            return games.count
+        default:
+            return teams.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "GameTableViewCell", for: indexPath) as! GameTableViewCell
         
-        cell.gameModel = games[indexPath.row]
-        
-        cell.setupCell()
-        
-        return cell
+        if stateTableVeiw == .games {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "GameTableViewCell", for: indexPath) as! GameTableViewCell
+            
+            cell.gameModel = games[indexPath.row]
+            
+            cell.setupCell()
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TeamTableViewCell", for: indexPath) as! TeamTableViewCell
+            var content = cell.defaultContentConfiguration()
+            let team = teams[indexPath.row]
+            content.image = UIImage(named: team)
+            content.text = team
+            
+            cell.contentConfiguration = content
+            
+            return cell
+        }
     }
-    
+}
+
+// MARK: - State Main Screen
+enum StateTableView {
+    case games
+    case teams
 }
